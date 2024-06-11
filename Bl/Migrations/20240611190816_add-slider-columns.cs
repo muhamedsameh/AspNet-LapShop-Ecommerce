70ba76c971588216f84500771ca5185793aa3480
mohamedsameh <mohamedsameh050@gmil.com>
2024-06-11 22:24:56 +0300

    Slider columns migrations

---
+++ Bl/Migrations/20240611190816_add-slider-columns.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LapShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class addslidercolumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "TbSlider",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "TbSlider",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "CurrentState",
+                table: "TbSlider",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "TbSlider",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "TbSlider",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "TbSlider");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "TbSlider");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentState",
+                table: "TbSlider");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "TbSlider");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "TbSlider");
+        }
+    }
+}
