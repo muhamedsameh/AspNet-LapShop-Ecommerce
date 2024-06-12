edeb990dcd54fab917e4e218e0d09e350910ce1f
mohamedsameh <mohamedsameh050@gmil.com>
2024-06-13 01:39:37 +0300

    update sittings api controller

---
+++ LapShop/APIControllers/SitttingsController.cs
@@ -0,0 +1,27 @@
+﻿using Domains;
+using LapShop.Bl;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LapShop.APIControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SitttingsController : ControllerBase
+    {
+        ISittings oClsSittings;
+        public SitttingsController(ISittings iSittings)
+        {
+            oClsSittings = iSittings;
+        }
+
+        // GET: api/<SitttingsController>
+        [HttpGet]
+        public TbSittings Get()
+        {
+            return oClsSittings.GetAll();
+        }
+
+    }
+}
