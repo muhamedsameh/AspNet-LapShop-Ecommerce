f7e0b811a205a17d34c49129de5c158cc74bb900
mohamedsameh <mohamedsameh050@gmil.com>
2024-06-12 04:45:57 +0300

    add categories to Homepage

--- LapShop/Controllers/HomeController.cs
+++ LapShop/Controllers/HomeController.cs
@@ -8,23 +8,26 @@ namespace LapShop.Controllers
     {
         IItems oClsItems;
         ISlider oClsSliders;
-        public HomeController(IItems item, ISlider slider)
+        ICategories oClsCategories;
+        public HomeController(IItems item, ISlider slider, ICategories iCategories)
         {
             oClsItems = item;
             oClsSliders = slider;
-
+            oClsCategories = iCategories;
         }
         public IActionResult Index()
         {
             VmHomePage vm = new VmHomePage();
+
             var allData = oClsItems.GetAllItemsData(null);
             vm.lstAllItems = allData.Take(18).ToList();
             vm.lstRecommendedItems = allData.Take(10).ToList();
             vm.lstNewItems = allData.Take(4).ToList();
             vm.lstFreeDeliveryItems = allData.Take(4).ToList();
 
-            var sliders = oClsSliders.GetAll().Take(4).ToList();
-            vm.lstSliderItems = sliders;
+            vm.lstSliderItems = oClsSliders.GetAll().Take(4).ToList();
+
+            vm.lstCategories = oClsCategories.GetAll().Take(4).ToList();
 
             return View(vm);
         }
