ce1f3dae698660e47695476160442cbbed54520e
mohamedsameh <mohamedsameh050@gmil.com>
2024-06-12 21:36:39 +0300

    add columns to sittings tabl

---
+++ Bl/Migrations/20240612183145_add-sitting-columns.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LapShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class addsittingcolumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "TbSittings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HomeBackgroundImgName",
+                table: "TbSittings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastPanner",
+                table: "TbSittings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "TbSittings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "phoneNumber",
+                table: "TbSittings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "TbSittings");
+
+            migrationBuilder.DropColumn(
+                name: "HomeBackgroundImgName",
+                table: "TbSittings");
+
+            migrationBuilder.DropColumn(
+                name: "LastPanner",
+                table: "TbSittings");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "TbSittings");
+
+            migrationBuilder.DropColumn(
+                name: "phoneNumber",
+                table: "TbSittings");
+        }
+    }
+}
