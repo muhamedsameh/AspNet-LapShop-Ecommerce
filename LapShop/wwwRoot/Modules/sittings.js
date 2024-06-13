46af6618dc075996f5041e106e9490c6e777ec61
mohamedsameh <mohamedsameh050@gmil.com>
2024-06-13 03:15:19 +0300

    update sitting.js

--- LapShop/wwwRoot/Modules/sittings.js
+++ LapShop/wwwRoot/Modules/sittings.js
@@ -2,16 +2,17 @@ var ClsSittings = {
     GetAll: function () {
         Helper.AjaxCallGet("https://localhost:7079/api/Sitttings", {}, "json",
             function (data) {
-                var htmlAllItems = "";
-
-                for (var i = 0; i < data.data.length; i++) {
-                    var htmlItem = ClsItems.DrawItem(data.data[i]);
-                    htmlAllItems += htmlItem;
-                }
-
-                // change inner html of itemArea elemnt
-                $("#ItemsArea").html(htmlAllItems);
+                console.log(data);
+                console.log(data.data);
+                console.log(data.FacebookLink);
+                $("#lnkFacebook").attr("href", data.data.FacebookLink);
+                $("#lnkYoutupe").attr("href", data.data.lnkYoutupe);
+                $("#lnkX").attr("href", data.data.XLink);
+                $("#lnkInsta").attr("href", data.data.InstaLink);
+                $("#lnkLinkedin").attr("href", data.data.LinkedinLink);
 
             }, function () { console.log("failed"); })
     }
 }
+
+ClsSittings.GetAll();
