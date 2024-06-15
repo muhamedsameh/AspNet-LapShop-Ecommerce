15172f567a18063d493115b5573ceb499a68145b
mohamedsameh <mohamedsameh050@gmil.com>
2024-06-15 09:23:04 +0300

    update sittings table

---
+++ Bl/Migrations/20240615061838_update-sittings-columns-names-and-set-constratins.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LapShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatesittingscolumnsnamesandsetconstratins : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discription",
+                table: "TbSittings");
+
+            migrationBuilder.RenameColumn(
+                name: "phoneNumber",
+                table: "TbSittings",
+                newName: "PhoneNumber");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "TbSittings",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "WebsiteName",
+                table: "TbSittings",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "TbSittings",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "TbSittings",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "TbSittings");
+
+            migrationBuilder.RenameColumn(
+                name: "PhoneNumber",
+                table: "TbSittings",
+                newName: "phoneNumber");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "WebsiteName",
+                table: "TbSittings",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "phoneNumber",
+                table: "TbSittings",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "TbSittings",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discription",
+                table: "TbSittings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- Domains/TbSittings.cs
+++ Domains/TbSittings.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,22 +10,59 @@ namespace Domains
 {
     public class TbSittings
     {
+        [ValidateNever]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Website Name is required")]
+        [StringLength(100, ErrorMessage = "Website Name cannot exceed 100 characters")]
         public string WebsiteName { get; set; }
+
+        [Required(ErrorMessage = "Logo is required")]
         public string Logo { get; set; }
-        public string Discription { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters")]
+        public string Description { get; set; }
+
+        [Url(ErrorMessage = "Please enter a valid Facebook URL")]
         public string FacebookLink { get; set; }
+
+        [Url(ErrorMessage = "Please enter a valid Twitter (X) URL")]
         public string XLink { get; set; }
+
+        [Url(ErrorMessage = "Please enter a valid Instagram URL")]
         public string InstaLink { get; set; }
-        public string YoutubeLink { get; set;}
-        public string LinkedinLink { get; set;}
+
+        [Url(ErrorMessage = "Please enter a valid YouTube URL")]
+        public string YoutubeLink { get; set; }
+
+        [Url(ErrorMessage = "Please enter a valid LinkedIn URL")]
+        public string LinkedinLink { get; set; }
+
+        [StringLength(200, ErrorMessage = "Location cannot exceed 200 characters")]
         public string Location { get; set; }
-        public string phoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [Phone(ErrorMessage = "Please enter a valid phone number")]
+        [StringLength(15, ErrorMessage = "Phone number cannot exceed 15 characters")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
-        public string MiddlePanner {  get; set; }
+
+        [Required(ErrorMessage = "Middle banner is required")]
+        public string MiddlePanner { get; set; }
+
+        [Required(ErrorMessage = "Last banner is required")]
         public string LastPanner { get; set; }
+
+        [Required(ErrorMessage = "Home background image is required")]
         public string HomeBackgroundImgName { get; set; }
+
+        [ValidateNever]
         public string UpdatedBy { get; set; }
+        [ValidateNever]
         public DateTime UpdatedDate { get; set; }
 
     }
