fc8d44224b6225184ae93f2e05b603129108804b
mohamedsameh <mohamedsameh050@gmil.com>
2024-06-18 10:53:41 +0300

    add validation data anotations

--- Domains/TbPages.cs
+++ Domains/TbPages.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,13 +13,20 @@ namespace Domains
         public int PageId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+
         public string MetaKeyWord { get; set; } = null!;
+
         public string MetaDescription { get; set; } = null!;
+        [ValidateNever]
         public string ImgName { get; set; } = null!;
+        [ValidateNever]
         public int CurrentState { get; set; }
         public DateTime CreatedDate { get; set; }
+        [ValidateNever]
         public string CreatedBy { get; set; } = null!;
+        [ValidateNever]
         public DateTime? UpdatedDate { get; set; }
+        [ValidateNever]
         public string? UpdatedBy { get; set; }
 
     }
